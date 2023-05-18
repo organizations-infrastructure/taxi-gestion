a38182998f1908f3836d3e5b20772a686c892dd0
Romain Cambonie <romain.cambonie@gmail.com>
2023-05-18 15:28:29 +0200

    feat: added discord hooks by type

--- variables.tf
+++ variables.tf
@@ -12,8 +12,29 @@ variable "tfe_token" {
   sensitive   = true
 }
 
-variable "notification_webhook" {
-  description = "A webhook to notify about deployments"
+variable "github_pat" {
+  description = "A GitHub Personal Access Token from the admin"
+  nullable    = false
+  default     = false
+  sensitive   = true
+}
+
+variable "notification_webhook_failures" {
+  description = "A webhook to notify about failed ci"
+  nullable    = false
+  default     = false
+  sensitive   = true
+}
+
+variable "notification_webhook_successes" {
+  description = "A webhook to notify about successful ci"
+  nullable    = false
+  default     = false
+  sensitive   = true
+}
+
+variable "notification_webhook_releases" {
+  description = "A webhook to notify about repository release"
   nullable    = false
   default     = false
   sensitive   = true
